a5c5aad8139dcbe017b165b2467f63953ebf71a7
Renj <itrenjunhua@163.com>
2018-01-22 23:57:09 +0800

    增加保存、获取字符串和字节数组的方法

--- cachelibrary/src/main/java/com/renj/cachelibrary/CacheManageUtils.java
+++ cachelibrary/src/main/java/com/renj/cachelibrary/CacheManageUtils.java
@@ -2,8 +2,17 @@ package com.renj.cachelibrary;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * ======================================================================
@@ -84,5 +93,176 @@ public class CacheManageUtils {
             throw new IllegalStateException("没有对 CacheManageUtils 进行初始化，需要先调用 CacheManageUtils.initCacheUtil(Context) " +
                     "或 CacheManageUtils.initCacheUtil(Context，String) 方法。建议在 Application 中调用。");
         return instance;
+    }
+
+    /**
+     * 缓存字符串({@link String})内容
+     *
+     * @param key   缓存键名称，同时用于获取缓存
+     * @param value 需要缓存的字符串内容({@link String})
+     * @return 保存的文件 {@link File} 对象 <b>注意：可能为 {@code null}</b>
+     * @see #put(String, String, long)
+     * @see #putOnNewThread(String, String)
+     * @see #putOnNewThread(String, String, long)
+     */
+    public File put(@NonNull String key, @NonNull String value) {
+        if (TextUtils.isEmpty(value)) return null;
+
+        File file = RCacheOperatorUtils.spliceFile(key);
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(value);
+            bufferedWriter.flush();
+
+            // 检查缓存大小
+            RCacheOperatorUtils.checkCacheSize();
+
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 缓存字节数组(byte[])
+     *
+     * @param key   缓存键名称，同时用于获取缓存
+     * @param bytes 需要缓存的字节数组(byte[])
+     * @return 保存的文件 {@link File} 对象 <b>注意：可能为 {@code null}</b>
+     * @see #put(String, byte[], long)
+     * @see #putOnNewThread(String, byte[])
+     * @see #putOnNewThread(String, byte[], long)
+     */
+    public File put(@NonNull String key, @NonNull byte[] bytes) {
+        if (bytes == null || bytes.length == 0) return null;
+
+        File file = RCacheOperatorUtils.spliceFile(key);
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(bytes);
+            fileOutputStream.flush();
+
+            // 检查缓存大小
+            RCacheOperatorUtils.checkCacheSize();
+
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取缓存的字符串内容({@link String})，没有则返回 {@code ""}
+     *
+     * @param key 缓存时的键名称
+     * @return 缓存的字符串内容 ({@link String})，没有则返回 {@code ""}
+     * @see #getAsStringOnNewThread(String)
+     */
+    public String getAsString(@NonNull String key) {
+        File file = RCacheOperatorUtils.spliceFile(key);
+        if (!file.exists()) return "";
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            String tResult = stringBuilder.toString();
+            // 是否包含有效期
+            if (RCacheOperatorUtils.isTimeLimit(tResult)) {
+                String resultVaule = RCacheOperatorUtils.clearDateInfo(tResult);
+                // 判断是否过期，如果已过期就删除该文件
+                if (RCacheConfig.OUT_TIME_FLAG.equals(resultVaule)) {
+                    RCacheOperatorUtils.deleteFile(file);
+                    return "";
+                } else {
+                    return resultVaule;
+                }
+            }
+
+            return tResult;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取缓存的字节数组(byte[])，没有则返回 {@code null}
+     *
+     * @param key 缓存时的键名称
+     * @return 缓存的字节数组(byte[])，没有则返回 {@code null}
+     * @see #getAsBinaryOnNewThread(String)
+     */
+    public byte[] getAsBinary(@NonNull String key) {
+        File file = RCacheOperatorUtils.spliceFile(key);
+        if (!file.exists()) return null;
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] bys = new byte[1024 * 5];
+            int len = 0;
+            while ((len = fileInputStream.read(bys)) != -1) {
+                outputStream.write(bys, 0, len);
+            }
+            byte[] readResult = outputStream.toByteArray();
+            // 是否包含有效期
+            if (RCacheOperatorUtils.isTimeLimit(readResult)) {
+                byte[] resultValue = RCacheOperatorUtils.clearDateInfo(readResult);
+                // 获取分隔符的字节形式
+                byte[] outTimeBytes = RCacheOperatorUtils.toBytes(RCacheConfig.OUT_TIME_FLAG);
+                // 判断是否过期，如果已过期就删除该文件
+                if (RCacheOperatorUtils.equalsBytes(outTimeBytes, resultValue)) {
+                    RCacheOperatorUtils.deleteFile(file);
+                    return null;
+                } else {
+                    return resultValue;
+                }
+            }
+            return readResult;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
